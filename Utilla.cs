00e4c3bc77cf5129bcaad6a9ddcff8b6901e989f
Kirt <87604221+Kirnt@users.noreply.github.com>
2023-07-01 08:38:52 +0100

    Add files via upload

---
+++ Utilla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BepInEx;
+using Utilla.HarmonyPatches;
+using Utilla.Utils;
+using UnityEngine;
+using System.Linq;
+using Photon.Realtime;
+using Photon.Pun;
+
+namespace Utilla
+{
+
+	[BepInPlugin("org.legoandmars.gorillatag.utilla", "Utilla", "1.6.8")]
+    public class Utilla : BaseUnityPlugin
+    {
+        static Events events = new Events();
+
+        void Start()
+        {
+            RoomUtils.RoomCode = RoomUtils.RandomString(6); // Generate a random room code in case we need it
+
+            GameObject dataObject = new GameObject();
+            DontDestroyOnLoad(dataObject);
+            gameObject.AddComponent<UtillaNetworkController>();
+
+            Events.GameInitialized += PostInitialized;
+
+            UtillaNetworkController.events = events;
+            PostInitializedPatch.events = events;
+
+            UtillaPatches.ApplyHarmonyPatches();
+        }
+
+        void PostInitialized(object sender, EventArgs e)
+		{
+			new GameObject("CustomGamemodesManager").AddComponent<GamemodeManager>();
+		}
+    }
+}
